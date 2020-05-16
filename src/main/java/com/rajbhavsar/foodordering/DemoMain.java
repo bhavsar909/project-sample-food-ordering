d9c7e8ab1512c6a2acfd765444363d622159e492
“rajbhavsar” <raj.bhavsar@brevitaz.com>
2020-05-16 11:10:37 +0530

    base commit.

---
+++ src/main/java/com/rajbhavsar/foodordering/DemoMain.java
@@ -0,0 +1,16 @@
+package com.rajbhavsar.foodordering;
+
+import com.rajbhavsar.foodordering.controller.ItemController;
+import com.rajbhavsar.foodordering.controller.UserController;
+import com.rajbhavsar.foodordering.controller.impl.ItemControllerImpl;
+import com.rajbhavsar.foodordering.controller.impl.UserControllerImpl;
+import com.rajbhavsar.foodordering.model.Item;
+import com.rajbhavsar.foodordering.model.User;
+
+public class DemoMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test setup");
+	}
+}
